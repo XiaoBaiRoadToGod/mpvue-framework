d1c5eff4b93ceb0b518d6d8c1f6a4bbf21cf4837
zjVirus <285857527@qq.com>
2019-12-11 17:39:14 +0800

    去掉console

--- src/utils/require-context.js
+++ src/utils/require-context.js
@@ -4,29 +4,22 @@ module.exports = function(directory, recursive, regExp) {
   
     // Assume absolute path by default
     var basepath = directory
-    console.log(directory)
-    console.log(path.isAbsolute(directory))
     if (directory[0] === '.') {
       // Relative path
-        console.log('......')
       basepath = path.join(__dirname, directory)
     } else if (!path.isAbsolute(directory)) {
       // Module path
-        console.log('isAbsolute')
       basepath = require.resolve(directory)
     }
-  console.log('require-context')
     var keys = dir
       .files(basepath, {
         sync: true,
         recursive: recursive || false
       })
         .filter(function (file) {
-          console.log(file)
         return file.match(regExp || /\.(json|js)$/)
       })
         .map(function (file) {
-            console.log(file)
             // console.log(path.join('.', file.slice(basepath.length)))
         return path.join('.', file.slice(basepath.length))
         // return path.join('.', file.slice(basepath.length + 1))
